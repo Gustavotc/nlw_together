512c5f16d924eb4e16bd284e6edfdeb445fa5b6e
Gustavo <gustavotch@hotmail.com>
2021-07-10 21:39:34 -0300

    Discord OAuth2 authentication implementation

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,20 +1,25 @@
 import React from 'react'
-import { View, Text, Image, } from 'react-native';
+import { View, Text, Image, Alert, ActivityIndicator } from 'react-native';
 
-import { useNavigation } from '@react-navigation/native';
+import { useAuth } from '../../hooks/auth'
 
 import IllustrationImg from '../../assets/illustration.png'
 import { styles } from './style';
 
 import { ButtonIcon } from '../../components/ButtonIcon';
 import { Background } from '../../components/Background';
+import { theme } from '../../global/styles/theme';
 
 
 export function SignIn() {
-  const navigation = useNavigation();
+  const { loading, signIn } = useAuth();
 
-  function handleSignIn() {
-    navigation.navigate('Home');
+  async function handleSignIn() {
+    try {
+      await signIn();
+    }catch (error) {
+      //Alert.alert(error);
+    }
   }
 
   return (
@@ -38,11 +43,15 @@ export function SignIn() {
             favoritos com seus amigos
           </Text>
 
-          <ButtonIcon
+          {
+            loading 
+            ? <ActivityIndicator color={theme.colors.primary}/> :
+            <ButtonIcon
             title="Entrar com Discord"
             onPress={handleSignIn}
           />
-
+          }
+          
         </View>
       </View>
     </Background>
